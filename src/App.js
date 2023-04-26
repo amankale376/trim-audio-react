6938436fecfe140dba600ce467bd5ea96a7ebf36
Amankale376 <amankale@rapidinnovation.dev>
2023-04-26 17:00:12 +0530

    Feat/ audio playback when region changes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import React, { useRef, useState } from 'react';
 import './App.css';
 import lamejs from 'lamejs';
 import RegionsPlugin from 'wavesurfer.js/dist/plugin/wavesurfer.regions.min.js';
-
 // use exact same module version as lamejs and Wavesurfer shows issues realted to pcm data. 
 //Error: Mpeg not set. this error shows in newest version of lamejs
 
 function App() {
   const [waveSurfer, setWaveSurfer] = useState(null);
+  const [regionPlaying, setRegionPlaying] = useState(false);
   const [audioFile, setAudioFile] = useState(null);
   const [playing, setPlaying] = useState(false);
   const waveformRef = useRef(null);
@@ -27,9 +27,9 @@ React.useEffect(()=>{
   if (!waveSurfer) {
     const options = {
       container: waveformRef.current,
-      waveColor: 'gray',
-      progressColor: 'orange',
-      cursorColor: 'black',
+      waveColor: 'black',
+      progressColor: 'grey',
+      cursorColor: 'white',
       barWidth: 2,
       barHeight: 1,
       barGap: null,
@@ -37,65 +37,65 @@ React.useEffect(()=>{
       height: 150,
       plugins: [
         RegionsPlugin.create({
-          regionsMinLength: 0.1,
-          // limitRegions: true,
+          limitRegions: true,
           dragSelection: {
             slop: 5,
           },
+          drag: true,
+          resize: true,
+          maxLength: 20,
+          minLength: 5
         }),
       ],
     };
     const ws = WaveSurfer.create(options);
     setWaveSurfer(ws);
-    // Add event listener inside the 'wavesurfer-ready' event
-
-    const addRegionRestriction = () => {
-      if (ws.regions.list.length >= 1) {
-        const lastRegion = ws.regions.list[ws.regions.list.length - 1];
-        console.log({lastRegion});
-        ws.regions.list.forEach(region => {
-          if (region.id !== lastRegion.id) {
-            ws.regions.list.splice(ws.regions.list.indexOf(region), 1);
-            console.log('region removing')
-            region.remove();
-          }
-        });
-      }
-    };
-    ws.on('region-created', addRegionRestriction);
     
   }else{
     waveSurfer.on('region-created', function (region) {
-      console.log(region);
-      region.on('click', function () {
-        region.play();
-      });
-      // waveSurfer.enableDragSelection(false);
+      console.log(region,'region-created');
+      waveSurfer.clearRegions()
+    });
+
+    waveSurfer.on('region-updated', function (region) {
+      console.log(region,'region-updated');
+      playRegion(true);
     });
   }
 },[waveSurfer]);
 React.useEffect(() => {
   if (waveSurfer && audioFile) {
     waveSurfer.loadBlob(audioFile);
+    addFakeRegion();
   }
 }, [waveSurfer, audioFile]);
 
 function addFakeRegion(){
   const region = waveSurfer.addRegion({
-    start: 1,
-    end: 3,
+    start: 10,
+    end: 30,
     color: 'rgba(255, 0, 0, 0.3)',
     drag: true,
     resize: true,
-    loop: false
+    loop: false,
+    maxLength: 20,
+    minLength: 5
   });
   console.log(region);
 }
-function cutRegion() {
-  console.log(waveSurfer.regions,'regions');
+function playRegion(regionAdjust = false) {
   for (const id in waveSurfer.regions.list) {
     const region = waveSurfer.regions.list[id];
-    region.play();
+    console.log({regionPlaying});
+    if(!regionPlaying){
+      region.playLoop();
+      setRegionPlaying(true);
+    }else if(regionPlaying){
+      waveSurfer.pause();
+      setRegionPlaying(false);
+    } else if(regionAdjust){
+      region.playLoop();
+    }
       break; // Exit loop after first active region is found
     
   }
@@ -111,7 +111,7 @@ async function downloadRegion() {
     const endIndex = Math.ceil(end * sampleRate);
     const buffer = await waveSurfer.backend.buffer;
     const clippedBuffer = buffer.getChannelData(0).subarray(startIndex, endIndex);
-    const mp3encoder = new lamejs.Mp3Encoder(1, sampleRate, 128);
+    const mp3encoder = new lamejs.Mp3Encoder(1, sampleRate, 320);
     let mp3Data = [];
     const samples = clippedBuffer.length;
     for (let i = 0; i < samples; i++) {
@@ -123,7 +123,7 @@ async function downloadRegion() {
     const url = URL.createObjectURL(blob);
     const link = document.createElement('a');
     link.href = url;
-    link.download = 'selected_region.mp3';
+    link.download = 'export-trimmed-audio.mp3';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
@@ -132,6 +132,7 @@ async function downloadRegion() {
     console.log('No region selected');
   }
 }
+
   return (
     <div className="App">
   <div>
@@ -140,7 +141,7 @@ async function downloadRegion() {
     </div>
     <div>
     <button type='button' onClick={playAudio} >Play</button>
-      <button onClick={cutRegion}>Cut Region</button>
+      <button onClick={playRegion}>Play Region</button>
       <button onClick={addFakeRegion}>Fake Region</button>
       <button onClick={downloadRegion}>Download Region</button>
     </div>
